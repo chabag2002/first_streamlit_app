fadc1ac7927d2e2067cb8dd29692cdb523f1b827
pradeep <109863198+chabag2002@users.noreply.github.com>
2022-07-23 23:27:54 -0400

    Update streamlit_app.py
    added content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit
 
-streamlit.title('Learn Python inn 1 week')
+streamlit.title('Learn Python in 1 week')
 
 streamlit.header('chapter 1: basics')
 
 
 streamlit.text('Learn about variables')
-streamlit.text('Learn about Loops')
+streamlt.text('Learn about Loops')
 
 
