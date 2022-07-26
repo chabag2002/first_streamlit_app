5ed549f7ab7ccd52d8abe669f8fbeb32d699ad2d
pradeep <109863198+chabag2002@users.noreply.github.com>
2022-07-26 07:40:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,13 @@ streamlit.header('Buils your own fruit smoothies')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.multiselect("Pick some fruits:" , list(my_fruit_list.index), ['Lime'])
+#picklist for teh fruits that are selected
+fruits_selected = streamlit.multiselect("Pick some fruits:" , list(my_fruit_list.index), ['Lime'])
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+#display teh table on teh page
+streamlit.dataframe( fruits_to_show)
 
 
 
